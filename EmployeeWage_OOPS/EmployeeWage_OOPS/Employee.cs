f50ca5282adc9daf5ae624dcc96f025190d719ad
Arshad <mohammedarshad183@gmail.com>
2022-05-07 18:07:13 +0530

    Add: creating employee wage

---
+++ EmployeeWage_OOPS/EmployeeWage_OOPS/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage_OOPS
+{
+    class Employee
+    {
+        public void Calculate() {
+            Console.WriteLine("Welcome to Employee wage Computation program on Master branch");
+        }
+    }
+}
