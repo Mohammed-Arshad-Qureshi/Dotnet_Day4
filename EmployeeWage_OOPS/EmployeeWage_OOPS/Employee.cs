2cd862b5b14e08053b65c5d6280a4c6b2903b0fa
Arshad <mohammedarshad183@gmail.com>
2022-05-07 18:20:55 +0530

    Added: Added Partimer

--- EmployeeWage_OOPS/EmployeeWage_OOPS/Employee.cs
+++ EmployeeWage_OOPS/EmployeeWage_OOPS/Employee.cs
@@ -9,14 +9,17 @@ namespace EmployeeWage_OOPS
     class Employee
     {
         public void Calculate() {
-            int Is_Present = 1;
+            int Is_Part_Time = 2;
+            int Is_Full_Time = 1;
             int Wage_Per_Hr = 20;
             int Daily_Wage = 0;
             int WorkingHrs = 0;
             Random r = new Random();
-            int check = r.Next(0, 2);
-            if (Is_Present == check)
+            int check = r.Next(0, 3);
+            if (Is_Full_Time == check)
                 WorkingHrs = 8;
+            if (Is_Part_Time == check)
+                WorkingHrs = 4;
             else
                 WorkingHrs = 0;
             Daily_Wage = WorkingHrs + Wage_Per_Hr;
